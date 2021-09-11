f065b04cdad4fe23ced92163dc2a1829fe4e6ce1
vobyt13 <88980041+vobyt13@users.noreply.github.com>
2021-09-11 18:24:59 -0500

    Add files via upload

--- binarysearchtreeeee.js
+++ binarysearchtreeeee.js
@@ -1,9 +1,6 @@
-// c++ professor harris this one's for you. thank you.
-// freecodecamp.
-
 class Node {
-    constructor(item) {
-        this.value = item;
+    constructor(value) {
+        this.value = value;
         this.left = null;
         this.right = null;
     }
@@ -14,83 +11,61 @@ class BinarySearchTree {
         this.root = null;
     }
     add(value) {
-        let node = new Node(value);
-
+       
         if(!this.root) {
-            this.root = node;
+            this.root = new Node(value);
             return undefined;
         }
-        var pointer = this.root;
-
-        while(pointer) {
-            if(node.value < pointer.value) {
-                if(!pointer.left) {
-                    pointer.left = node;
+        var curr = this.root;
+        while(curr) {
+            if(curr.value < value) {
+                if(!curr.left) {
+                    curr.left = new Node(value);
                     return undefined;
                 }
-                pointer = pointer.left;
-            } else if(node.value > pointer.value) {
-                if(!pointer.right) {
-                    pointer.right = node;
+                curr = curr.left;
+            } else if(curr.value > value) {
+                if(!curr.right) {
+                    curr.right = new Node(value);
                     return undefined;
                 }
-                pointer = pointer.right;
             } else {
                 return null;
             }
         }
     }
-    findMin() {
-        if(!this.root) {
-            return null;
-        }
+    delete(value) {
+        var curr = this.root;
+        while(curr) {
+            if(curr.value === value) {
+                let right = curr.right;
+                let left = curr.left;
+                this.root = curr.right;
 
-        var pointer = this.root;
-        while(pointer) {
-            if(pointer.left) {
-                pointer = pointer.left;
-            }
-        }
-        return pointer.value;
-    }
-    findMax() {
-        if(!this.root) {
-            return null;
-        }
-        var pointer = this.root;
-        while(pointer.right) {
-            pointer = pointer.right;
-        }
-        return pointer.value;
-
-    }
-    isPresent(value) {
-        if(!this.root) {
-            return null;
-        }
-
-        var pointer = this.root;
-        while(pointer) {
-            if(value == pointer.value) {
-                return true;
-            } else {
-                if(value < pointer.value) {
-                    pointer = pointer.left;
-                } else if(value > pointer.value) {
-                    pointer = pointer.right;
+                var pointer = this.root;
+                while(pointer) {
+                    if(pointer.right === null) {
+                        pointer.right = left;
+                    }
+                }
+            } else if(value < curr.value)  {
+                if(curr.left) {
+                    curr = curr.left;
+                }
+            } else if(value > curr.value) {
+                if(curr.right) {
+                    curr = curr.right;
                 }
             }
-            
         }
     }
 }
 
-
 const bst = new BinarySearchTree();
-bst.add(1);
-bst.add(4);
 bst.add(5);
-bst.add(8);
+bst.add(10);
+bst.add(13);
+bst.add(28);
+bst.add(22);
 
-let ts = bst.isPresent(5);
-console.log(ts);
+bst.delete(13);
